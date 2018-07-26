601410b22da98e1481e133a8a602c1c06edc2000
sirwin4 <sean.irwinxc@gmail.com>
2018-07-26 13:11:04 -0500

    added second FK to Order

--- bangazon/API/models.py
+++ bangazon/API/models.py
@@ -21,6 +21,6 @@ class Product(models.Model):
 class Order(models.Model):
     order_name = models.CharField(max_length=30)
     product_id = models.ForeignKey(Product, on_delete=models.PROTECT, null=True, blank=True)
-    # customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_id = models.ForeignKey(Customer, on_delete=models.PROTECT, null=True, blank=True)
     # payment_id = models.ForeignKey(Payment, default=Null)
 
