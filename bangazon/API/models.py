a844ea62adf4152836a0f55db127a12e84682f08
sirwin4 <sean.irwinxc@gmail.com>
2018-07-26 10:18:13 -0500

    fixed price

--- bangazon/API/models.py
+++ bangazon/API/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=30)
-    price = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     description = models.CharField(max_length=250)
     quantity = models.IntegerField(default=1)
     # customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
