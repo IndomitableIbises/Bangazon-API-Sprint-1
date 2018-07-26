7a279b44bc737e5307b6c2d1de40a79926e73b88
jcarter0149 <jacobraycarter@gmail.com>
2018-07-26 10:14:26 -0500

    corrected the misspelled word

--- bangazon/API/serializers.py
+++ bangazon/API/serializers.py
@@ -4,7 +4,7 @@ from API.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'price', 'description', 'quanity')
+        fields = ('id', 'title', 'price', 'description', 'quantity')
 
 
 
