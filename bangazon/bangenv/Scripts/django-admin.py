c30355bc4d2a038fd9550d30a1b8ba4e1bfb17ed
CashewRose <danieagnoletti@gmail.com>
2018-07-26 10:08:47 -0500

    Deleted duplicate virtual environment from master

--- bangazon/bangenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\raf\workspace\bangazon-api-sprint-1\bangazon\bangenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
