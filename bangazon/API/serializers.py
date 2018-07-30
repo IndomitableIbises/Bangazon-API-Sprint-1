05f56d7d840e70b4ed59058cb860ff41ec72c0f5
rafcevallos <rafael.cevallos81@gmail.com>
2018-07-30 11:55:08 -0500

    Uncommented all foreign keys.  Made changes per the group's decision.

--- bangazon/API/serializers.py
+++ bangazon/API/serializers.py
@@ -1,25 +1,11 @@
 
 from rest_framework import serializers
-from API.models import Product, Customer, Product_Type, Payment_Type, Order, Employee, Training_Prog, Emp_Training
-from API.models import Product, Customer, Product_Type, Computer
-from API.models import Product, Customer, Department
-from API.models import Product, Customer, Product_Type, Order
-
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = ('id', 'account_date', 'active', 'last_login', 'first_name', 'last_name')
-
-from API.models import Product, Customer, Product_Type, Payment_Type, Order
-from API.models import Product, Customer, Product_Type, Payment_Type, Order, Employee
+from API.models import Product, Customer, Product_Type, Payment_Type, Order, Employee, Training_Prog, Emp_Training, Department, Computer
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'title', 'price', 'description', 'quantity', 'customer_id', 'type_id')
-
-
+###########################################################################
+# CUSTOMER SIDE SERIALIZERS
+###########################################################################
 
 # Authors Raf and Cashew <3
 # Translates customer table database into json format
@@ -28,30 +14,16 @@ class CustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = ('id', 'account_date', 'active', 'last_login', 'first_name', 'last_name')
 
-# Translates database into json format
-
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Department
-        fields = ('id', 'name', 'budget')
-
-        # 'supervisor_id'
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ('id', 'order_name', 'product_id', 'customer_id')
-
-
 class ProductTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product_Type
         fields = ('id', 'name')
 
-class ComputerSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Computer
-        fields = ('id', 'purchase_date', 'decom_date')
+        model = Product
+        fields = ('id', 'title', 'price', 'description', 'quantity', 'customer_id', 'type_id')
+
 # Author Cashew <3
 # Translates customer table database into json format
 class PaymentTypeSerializer(serializers.ModelSerializer):
@@ -59,16 +31,26 @@ class PaymentTypeSerializer(serializers.ModelSerializer):
         model = Payment_Type
         fields = ('id', 'name', 'account_num', 'customer_id')
 
-
-###########################################################################
-# EMPLOYEE SIDE VIEWS
-###########################################################################
-
-# Author Raf - EmployeeSerializer
-class EmployeeSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Employee
-        fields = '__all__' # this should automatically pipe in all field names and values once foreign keys in Employee Model are uncommented
+        model = Order
+        fields = ('id', 'order_name', 'product_id', 'customer_id', 'payment_id')
+
+
+###########################################################################
+# EMPLOYEE SIDE SERIALIZERS
+###########################################################################
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ('id', 'name', 'budget', 'supervisor_id')
+
+class ComputerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Computer
+        fields = ('id', 'purchase_date', 'decom_date')
 
 # Authors: Cashew & Raf <3 - TrainingSerializer
 class TrainingSerializer(serializers.ModelSerializer):
@@ -81,3 +63,9 @@ class Emp_TrainSerializer(serializers.ModelSerializer):
     class Meta:
         model = Emp_Training
         fields = '__all__'
+
+# Author Raf - EmployeeSerializer
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__' # this should automatically pipe in all field names and values once foreign keys in Employee Model are uncommented
