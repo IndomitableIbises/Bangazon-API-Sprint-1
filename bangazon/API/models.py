1553d812b881685175e49f0ef1499bd3411ea91f
jcarter0149 <jacobraycarter@gmail.com>
2018-07-30 10:07:11 -0500

    changed the Decom date to a datefield

--- bangazon/API/models.py
+++ bangazon/API/models.py
@@ -26,7 +26,7 @@ class Product_Type(models.Model):
 
 class Computer(models.Model):
     purchase_date = models.DateTimeField(auto_now_add=True)
-    decom_date = models.IntegerField(default=1)
+    decom_date = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     ## Shows in field when you grab the foreign key the name of the product type
     def __str__(self):
         return f'{self.name}'
